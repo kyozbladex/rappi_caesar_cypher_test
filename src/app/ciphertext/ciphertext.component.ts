d40686898ada805ffc8af25993277d362304bea0
kyozbladex <72166766+kyozbladex@users.noreply.github.com>
2020-12-07 21:18:55 -0400

    preparando otro branch

--- src/app/ciphertext/ciphertext.component.ts
+++ src/app/ciphertext/ciphertext.component.ts
@@ -34,7 +34,46 @@ export class CiphertextComponent {
 
   }
 
-  getPlainDisplayText(evento) {
+  getPlainDisplayText(text, shifter) {
+
+      let newString = [];
+
+   // newString = text.split('');
+
+   // console.log(newString);
+
+    shifter = shifter % 26;
+
+   // console.log(shifter);
+
+    for (let i = 0; i < text.length; i++) {
+
+      let charCode = text[i].charCodeAt();
+      let newCharCode = 0;
+      
+      if (charCode >= 65 && charCode <= 90) {
+        // Only then do a alphabet rotation
+        newCharCode = charCode + shifter;
+        if (newCharCode > 90) {
+            newCharCode = (newCharCode - 90) + 64;
+        }
+
+    } else if (charCode >= 97 && charCode <= 122) {
+        // Only then do a alphabet rotation
+        newCharCode = charCode + shifter;
+        if (newCharCode > 122) {
+            newCharCode = (newCharCode - 122) + 96;
+        }
+
+    } else {
+        newCharCode = charCode;
+    }
+  
+    newString.push(String.fromCharCode(newCharCode));
+
+    }
+
+    return newString.join("").trim();
 
   }
 
--- src/app/plaintext/plaintext.component.ts
+++ src/app/plaintext/plaintext.component.ts
@@ -10,12 +10,19 @@ export class PlaintextComponent {
   plaintext: string;
   @Input() plainDisplayText: string = "";
   cipherDisplayText: string = "";
+  @Input() numeroShifter: number;
+  shift: number;
 
 
   constructor(public service: ShiftValueService) { 
               this.service.textoAdecifrar.subscribe(
                 (plainDisplayText) => {
                   this.plaintext=plainDisplayText
+                }
+              );
+              this.service.numeroShift.subscribe(
+                (numeroShifter) => {
+                  this.shift = numeroShifter
                 }
               );
   }
@@ -28,13 +35,46 @@ export class PlaintextComponent {
 
   }
 
-  getCipherDisplayText() {
+  getCipherDisplayText(text, shifter) {
+    let newString = [];
+
+   // newString = text.split('');
+
+   // console.log(newString);
+
+    shifter = shifter % 26;
+
+   // console.log(shifter);
+
+    for (let i = 0; i < text.length; i++) {
+
+      let charCode = text[i].charCodeAt();
+      let newCharCode = 0;
+      
+      if (charCode >= 65 && charCode <= 90) {
+        // Only then do a alphabet rotation
+        newCharCode = charCode - shifter;
+        if (newCharCode > 90) {
+            newCharCode = (newCharCode - 90) + 64;
+        }
+
+    } else if (charCode >= 97 && charCode <= 122) {
+        // Only then do a alphabet rotation
+        newCharCode = charCode - shifter;
+        if (newCharCode > 122) {
+            newCharCode = (newCharCode - 122) + 96;
+        }
+
+    } else {
+        newCharCode = charCode;
+    }
+  
+    newString.push(String.fromCharCode(newCharCode));
+
+    }
+
+    return newString.join("").trim();
   }
 
-
-  verificarTexto(texto){
-    console.log(texto);
-    var textoprueba = texto;
-  }
 
 }
